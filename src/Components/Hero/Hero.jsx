d8cd4b34bd37125bd7fb6d4503226a73b0f91d67
VishuAlugade <alugadevishwjeet@gmail.com>
2025-08-14 18:05:13 +0530

    update portfolio

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1><span>I'm Vishwajeet Alugade,</span> frontend developer based in India.</h1>
         <p>"Frontend developer building responsive, user-friendly websites using HTML, CSS, JavaScript, and React to deliver engaging digital experiences."</p>
         <div className="hero-action">
-            <div className="hero-connect">LinkedIn</div>
+            <div className="hero-connect"><a href="https://www.linkedin.com/in/vishwajeet-alugade/">LinkedIn</a></div>
             <div className="hero-resume">My resume</div>
             
         </div>
