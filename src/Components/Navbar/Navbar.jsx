75ad5b29e808c7e08d0a0d1cbc150a8cda17b936
VishuAlugade <alugadevishwjeet@gmail.com>
2025-08-14 17:14:36 +0530

    update portfolio

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,56 @@
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import './Navbar.css'
+import { useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import './Navbar.css';
+import menu_open from '../../assets/menu_open.svg';
+import menu_close from '../../assets/menu_close.svg';
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='navbar'>
       <div className="navbar-title">
         <h1>Portfolio</h1>
       </div>
-        <ul className="nav-menu">
-            <li><AnchorLink className='anchor-link' href='#home'><p>Home</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#education'><p>Education</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#projects'><p>Projects</p></AnchorLink></li>
-            <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Contact</p></AnchorLink></li>
-        </ul>
-    </div>
-  )
-}
 
-export default Navbar
+      {/* Desktop Menu */}
+      <ul className="nav-menu desktop-menu">
+        <li><AnchorLink className='anchor-link' href='#home'><p>Home</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#education'><p>Education</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#projects'><p>Projects</p></AnchorLink></li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Contact</p></AnchorLink></li>
+      </ul>
+
+      {/* Menu Icon (Mobile) */}
+      <img
+        src={menu_open}
+        alt="menu"
+        className="menu-icon"
+        onClick={() => setMenuOpen(true)}
+      />
+
+      {/* Mobile Sidebar */}
+      <div className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
+        <div className="mobile-menu-header">
+          <h2>Menu</h2>
+          <img
+            src={menu_close}
+            alt="close"
+            className="close-icon"
+            onClick={() => setMenuOpen(false)}
+          />
+        </div>
+        <ul>
+          <li onClick={() => setMenuOpen(false)}><AnchorLink className='anchor-link' href='#home'><p>Home</p></AnchorLink></li>
+          <li onClick={() => setMenuOpen(false)}><AnchorLink className='anchor-link' offset={50} href='#about'><p>About Me</p></AnchorLink></li>
+          <li onClick={() => setMenuOpen(false)}><AnchorLink className='anchor-link' offset={50} href='#education'><p>Education</p></AnchorLink></li>
+          <li onClick={() => setMenuOpen(false)}><AnchorLink className='anchor-link' offset={50} href='#projects'><p>Projects</p></AnchorLink></li>
+          <li onClick={() => setMenuOpen(false)}><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Contact</p></AnchorLink></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
